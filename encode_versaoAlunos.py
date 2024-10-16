a78da309b80d7866bec3c00e4ea93b83721182bc
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2024-10-15 21:26:51 -0300

    emissor

--- encode_versaoAlunos.py
+++ encode_versaoAlunos.py
@@ -1,27 +1,9 @@
-
-#importe as bibliotecas
-from suaBibSignal import *
+# Importações necessárias
+from suaBibSignal import signalMeu
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
-
-#funções caso queriram usar para sair...
-def signal_handler(signal, frame):
-        print('You pressed Ctrl+C!')
-        sys.exit(0)
-
-#converte intensidade em Db, caso queiram ...
-def todB(s):
-    sdB = 10*np.log10(s)
-    return(sdB)
-
-
-
-
-def main():
-    
-   
-    #********************************************instruções*********************************************** 
+#********************************************instruções*********************************************** 
     # Seu objetivo aqui é gerar duas senoides. Cada uma com frequencia corresposndente à tecla pressionada, conforme tabela DTMF.
     # Então, inicialmente peça ao usuário para digitar uma tecla do teclado numérico DTMF.
     # De posse das duas frequeências, agora voce tem que gerar, por alguns segundos suficientes para a outra aplicação gravar o audio, duas senoides com as frequencias corresposndentes à tecla pressionada.
@@ -31,22 +13,64 @@ def main():
     # Some as duas senoides. A soma será o sinal a ser emitido.
     # Utilize a funcao da biblioteca sounddevice para reproduzir o som. Entenda seus argumento.
     # Você pode gravar o som com seu celular ou qualquer outro microfone para o lado receptor decodificar depois. Ou reproduzir enquanto o receptor já capta e decodifica.
-    
     # construa o gráfico do sinal emitido e o gráfico da transformada de Fourier. Cuidado, como as frequencias sao relativamente altas, voce deve plotar apenas alguns pontos (alguns periodos) para conseguirmos ver o sinal
-    
+# Frequências associadas a cada tecla DTMF
+# Frequências associadas a cada tecla DTMF, conforme a nova tabela
+dtmf_freqs = {
+    '1': (679, 1209), '2': (679, 1336), '3': (679, 1477), 'A': (679, 1633),
+    '4': (770, 1209), '5': (770, 1336), '6': (770, 1477), 'B': (770, 1633),
+    '7': (825, 1209), '8': (825, 1336), '9': (825, 1477), 'C': (825, 1633),
+    'X': (941, 1209), '0': (941, 1336), '#': (941, 1477), 'D': (941, 1633)
+}
+
+
+def main():
     print("Inicializando encoder")
     print("Aguardando usuário")
-    print("Gerando Tons base")
-    print("Executando as senoides (emitindo o som)")
-    print("Gerando Tom referente ao símbolo : {}".format(NUM))
-    sd.play(tone, fs)
-    # aguarda fim do audio
-    sd.wait()
     
-    plotFFT(self, signal, fs)
-    # Exibe gráficos
+    # Solicita tecla do usuário
+    tecla = input("Digite uma tecla do teclado numérico DTMF: ")
+    
+    # Frequências correspondentes à tecla
+    if tecla in dtmf_freqs:
+        f1, f2 = dtmf_freqs[tecla]
+        print(f"Gerando Tom referente ao símbolo : {tecla}")
+    else:
+        print("Tecla inválida! Execute o programa novamente.")
+        return
+    
+    # Parâmetros do sinal
+    fs = 44100  # Taxa de amostragem
+    duration = 5  # Duração em segundos
+    t = np.linspace(0, duration, int(fs * duration))
+    A = 1  # Amplitude
+    
+    # Geração das senoides
+    senoide1 = A * np.sin(2 * np.pi * f1 * t)
+    senoide2 = A * np.sin(2 * np.pi * f2 * t)
+    
+    # Soma das senoides para o sinal DTMF
+    tone = senoide1 + senoide2
+    
+    # Executa o som
+    print("Executando as senoides (emitindo o som)")
+    sd.play(tone, fs)
+    sd.wait()  # Aguarda a execução do áudio
+    
+    # Plota o sinal no domínio do tempo
+    plt.figure(figsize=(10, 4))
+    plt.plot(t[:1000], tone[:1000])  # Exibe os primeiros 1000 pontos para melhor visualização
+    plt.title(f'Sinal de Áudio para a Tecla {tecla}')
+    plt.xlabel('Tempo [s]')
+    plt.ylabel('Amplitude')
+    plt.grid()
     plt.show()
     
+    # Gera a Transformada de Fourier e plota
+    print("Gerando gráficos da Transformada de Fourier")
+    signal = signalMeu()  
+    signal.plotFFT(tone, fs)
+    plt.show()
 
 if __name__ == "__main__":
     main()
